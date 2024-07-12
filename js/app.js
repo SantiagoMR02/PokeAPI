73e930b2e745aed35f9523a166664809f4c8fc96
Julian Santiago Millan Rodriguez <126290894+SantiagoMR02@users.noreply.github.com>
2024-07-12 13:38:55 -0500

    Uso de Api

---
+++ js/app.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetchdata();
+});
+
+const fetchdata = async () => {
+    try {
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');//utilizacion de la API
+        const data = await res.json(); //Se obtiene el resultado de la llamada de la APi y se convierte a Json
+        const promises = data.results.map(async (pokemon) => {
+            const res = await fetch(pokemon.url);
+            return await res.json();
+        });
+        const detailedPokemonData = await Promise.all(promises);
+        const sortedPokemonData = detailedPokemonData.sort((a, b) => a.id - b.id);
+        sortedPokemonData.forEach(pokemon => dataCard(pokemon));
+    } catch (error) {
+        console.log(error);
+    }
+};
+//Recojo los datos de todas las debilidades del pokemon
+const fetchWeakness = async (typeUrl) => {
+    try {
+        const res = await fetch(typeUrl);
+        const typeData = await res.json();
+        const weaknesses = typeData.damage_relations.double_damage_from.map(type => type.name);
+        return weaknesses.slice(0, 2).join(', '); //Limito a 2 las debilidades
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const dataCard = async (pokemon) => {
+    const flex = document.querySelector('.flex');
+    const template = document.querySelector('#template-card').content;
+    const clone = template.cloneNode(true);
+    const fragment = document.createDocumentFragment();
+
+    const pokemonTypes = pokemon.types.map(type => type.type.name).join(', ');
+    const weaknesses = await fetchWeakness(pokemon.types[0].type.url);
+
+    clone.querySelector('.card-body-img').setAttribute('src', pokemon.sprites.other['official-artwork'].front_default);
+    clone.querySelector('.card-body-name').innerHTML = `#${pokemon.id} <span>${pokemon.name}</span>`;
+    clone.querySelector('.card-body-text').textContent = pokemonTypes;
+    clone.querySelector('.card-footer-power p').textContent = pokemon.stats.find(stat => stat.stat.name === 'attack').base_stat;
+    clone.querySelector('.card-footer-debility p').textContent = weaknesses;
+    clone.querySelector('.card-footer-peso p').textContent = `${pokemon.weight} KG`;
+
+    fragment.appendChild(clone);
+    flex.appendChild(fragment);
+};
